e7ce51b8fe768bca3a58d3a314975039564a782a
JaviThomas <dog2785@gmail.com>
2023-04-28 14:54:34 -0400

    Late updates?

--- Email_Sending_python.py
+++ Email_Sending_python.py
@@ -1,4 +1,3 @@
-
 import smtplib
 import pandas as pd
 import csv
@@ -6,12 +5,13 @@ import imghdr
 from email.message import EmailMessage
 
 
+
 # Your information here
 EMAIL_ADDRESS = ('Youremail@yahoo.com')
 EMAIL_PASSWORD = ('your3rdPartyPassword') # Note: This password is the 3rd party created password
 
 
-f = open('NameofFile.txt') #name of the email_list file
+f = open('Name_of_email_instruction_file.txt') #name of the email_list file
 
 csv_f = csv.reader(f)
 
@@ -25,13 +25,15 @@ for row in csv_f :
     msg['To'] =  ', '.join(contacts)
 
     msg.set_content('Attached to this email is your invoice, thank you for your business!') # Message Attached with Email
-
     with open(file_location, 'rb') as f:
         file_data = f.read()
-        file_name = f.name
+        file_name_list = f.name
+        file_name = file_name_list.split("\\")
+        file_name = file_name[-1]
 
     msg.add_attachment(file_data, maintype='application' , subtype='octet-stream', filename=file_name)
 
     with smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
+    
